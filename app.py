b29e265283c5075c881f7def43d86c7f2fd690cf
Asep Hendar <4sep.hendar@gmail.com>
2024-03-06 20:15:16 +0700

    Membuat method POST & parameter json

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from hackerrank import Find_Digits
+import json
 
 
 app = Flask(__name__)
@@ -8,6 +9,22 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def view_tugas_FD():
     response = Find_Digits.findDigits(1012)
+    
+    return jsonify({"results" : response})
+
+@app.route("/", methods=["POST"])
+def add_tugas_FD():
+    data = request.json["data"]
+    response = Find_Digits.findDigits(data)
+    hasil = json.dumps(response, indent=2)
+    
+    print("tipe data : ", type(data))
+    print("tipe response: ", type(response))
+    print("tipe json.dumps : ", type(hasil))
+    print("tipe request.json : ", type(request.json))
+    print("tipe jsonify : ", type(jsonify))
+    print("tipe dict: ", type({"results" : response}))
+    print("tipe f-str: ", type(f"{hasil}"))
     
     return jsonify({"results" : response})
 
